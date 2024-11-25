97ab248435ef3e0478862f0e34df5ca7ed615910
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-11-25 13:53:48 -0600

    Version 4.0 Add concurrency

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>New Version 3.0 Is Here!</h1>
+      <h1>New Version 4.0 Is Here!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
